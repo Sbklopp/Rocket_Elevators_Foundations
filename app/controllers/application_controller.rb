a2c24f04670072c6f35bcbc9c5c47867702ffa6e
Sbklopp <sbklopp11@gmail.com>
2022-04-12 22:00:25 -0400

    added skip before to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, raise: false
+  skip_after_action :verify_authorized
     rescue_from CanCan::AccessDenied do
         flash[:error] = 'Access denied!'
         redirect_to root_url
